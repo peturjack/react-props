b4f32591688d17c4206d6e8418715fc9a7e880f5
Petur jack <peturwj@gmail.com>
2023-10-26 15:27:40 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Card from "./components/card";
+import img1 from "./images/pexels-hasibullah-zhowandai-819530.jpg"
+import img2 from "./images/pexels-andrea-piacquadio-845457.jpg"
+import img3 from "./images/pexels-andrea-piacquadio-3781731.jpg"
+
+/*a array of objects*/
 function App() {
+  const blogs = [{ 
+    background: img1,
+    name: "John Doe",
+    about: "I'm a frontend developer, i love react and yarn, let's learn together!"
+  },{
+    background:img2,
+    name: "Andres Piacquadio",
+    about: "I'm a photographer and i love taking pictures of mountains"
+  },{
+    background:img3,
+    name: "Rachel Sky",
+    about: "I'm a Graphic Designer and i love creating something out of nothing!"
+  }]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {blogs.map((blog)=>{
+        return (<div key={blog.name}>
+          <Card background = {blog.background} name = {blog.name} about = {blog.about}></Card>
+          </div>)
+      })}
+    </>
   );
 }
 
